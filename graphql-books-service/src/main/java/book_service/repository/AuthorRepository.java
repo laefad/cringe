54822edc61a384c3b66e0afa8c08ad7e522c5606
laefad <FreeFlightA@yandex.ru>
2021-12-05 19:11:18 +0300

    memory cpu limit

--- graphql-books-service/src/main/java/book_service/repository/AuthorRepository.java
+++ graphql-books-service/src/main/java/book_service/repository/AuthorRepository.java
@@ -1,5 +1,14 @@
 package book_service.repository;
 
-public class AuthorRepository {
-    
+import book_service.model.Author;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
 }
