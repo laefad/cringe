7a070fdde097cc43e545100816f178aaef41f763
laefad <FreeFlightA@yandex.ru>
2021-12-09 21:19:34 +0300

    fix hystrics?

--- graphql-books-service/src/main/java/book_service/App.java
+++ graphql-books-service/src/main/java/book_service/App.java
@@ -3,10 +3,10 @@ package book_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableDiscoveryClient
 @EnableOAuth2Client
 @EnableFeignClients
-@EnableResourceServer
+@EnableCircuitBreaker
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableJpaRepositories
 public class App {
