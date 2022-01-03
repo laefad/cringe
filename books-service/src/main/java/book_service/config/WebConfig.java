beba43194c30348c4af38645543052ada5299438
laefad <FreeFlightA@yandex.ru>
2022-01-03 21:15:55 +0300

    update dep & add AuthoritiesConverter

--- books-service/src/main/java/book_service/config/WebConfig.java
+++ books-service/src/main/java/book_service/config/WebConfig.java
@@ -1,18 +1,22 @@
 package book_service.config;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
@@ -20,8 +24,11 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -43,7 +50,11 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                     )
                 )
             )
-            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+            .oauth2ResourceServer(resourceServerConfigurer -> resourceServerConfigurer
+                .jwt(jwtConfigurer -> jwtConfigurer
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+            )
+            // .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
             .oauth2Client()
             ;
     }
@@ -68,6 +79,45 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                     .collect(Collectors.toSet());
 
             return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
+    }
+
+    @Bean
+    public Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
+        JwtGrantedAuthoritiesConverter delegate = new JwtGrantedAuthoritiesConverter();
+
+        return new Converter<>() {
+            @Override
+            public Collection<GrantedAuthority> convert(Jwt jwt) {
+                Collection<GrantedAuthority> grantedAuthorities = delegate.convert(jwt);
+
+                if (jwt.getClaim("realm_access") == null) {
+                    return grantedAuthorities;
+                }
+                JSONObject realmAccess = jwt.getClaim("realm_access");
+                if (realmAccess.get("roles") == null) {
+                    return grantedAuthorities;
+                }
+                JSONArray roles = (JSONArray) realmAccess.get("roles");
+
+                final List<SimpleGrantedAuthority> keycloakAuthorities = 
+                    roles.stream()
+                         .map(role -> 
+                            new SimpleGrantedAuthority("ROLE_" + role)
+                         )
+                         .collect(Collectors.toList());
+
+                grantedAuthorities.addAll(keycloakAuthorities);
+
+                return grantedAuthorities;
+            }
         };
     }
 
